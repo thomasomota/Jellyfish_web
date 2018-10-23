01d30b0a13e6bec4191853c522c5c4fccbfafa36
thomasomota <thomas.ulvoen@gmail.com>
2018-10-23 21:46:58 +0200

    :sparkles: Reformate and introduce screen bounds

--- Frontend/webpagesandbox/src/threejs/SceneManager.js
+++ Frontend/webpagesandbox/src/threejs/SceneManager.js
@@ -55,7 +55,7 @@ export default canvas => {
     function createSceneSubjects(scene) {
         const sceneSubjects = [
             new GeneralLights(scene),
-            new SceneSubject(scene)
+            new SceneSubject(scene, screenDimensions)
         ];
 
         return sceneSubjects;
--- Frontend/webpagesandbox/src/threejs/SceneSubject.js
+++ Frontend/webpagesandbox/src/threejs/SceneSubject.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three'
 
-export default scene => {    
-    const group = new THREE.Group();
+export default (scene, screenDimensions) => {    
+    const jellyfish = new THREE.Object3D();
 
-    const jellyfish = new THREE.Mesh( new THREE.CubeGeometry( 20, 20, 20 ), new THREE.MeshLambertMaterial() );
+    const body = new THREE.Mesh( new THREE.CubeGeometry( 20, 20, 20 ), new THREE.MeshLambertMaterial() );
 
     const tentacle = new THREE.Mesh( new THREE.CubeGeometry( 1, 1, 30 ), new THREE.MeshLambertMaterial() );
     tentacle.position.set(9.5,9.5,10)
@@ -17,22 +17,71 @@ export default scene => {
     const tentacle_4 = new THREE.Mesh( new THREE.CubeGeometry( 1, 1, 30 ), new THREE.MeshLambertMaterial() );
     tentacle_4.position.set(-9.5,-9.5,10)
 
-    group.add(jellyfish)
-    group.add(tentacle)
-    group.add(tentacle_2)
-    group.add(tentacle_3)
-    group.add(tentacle_4)
+    jellyfish.add(body)
+    jellyfish.add(tentacle)
+    jellyfish.add(tentacle_2)
+    jellyfish.add(tentacle_3)
+    jellyfish.add(tentacle_4)
 
-    scene.add(group);
+    scene.add(jellyfish);
 
-    group.rotation.z = Math.PI/4;
-    const speed = 0.1;
+    jellyfish.position.x = 0;
+    jellyfish.position.y = 0;
+    jellyfish.position.z = 0;
+
+
+    jellyfish.rotation.z = Math.PI/4;
+    const speed = 0.3;
+
+    console.log(screenDimensions)
+
+    const screen_width = screenDimensions.screen_width;
+    const screen_height = screenDimensions.screen_height;
+
+    var jellyfish_direction_x = 0.1;
+    var jellyfish_direction_y = 0.1;
+    var jellyfish_direction_z = 0.1;
+
+    var jellyfish_velocity = Math.random() * 50;
 
     function update(time) {
+
+        //console.log("old x: ", jellyfish.position.x )
+        //console.log("old y: ", jellyfish.position.y )
+
+        //const x_pos = jellyfish.position.x;
+        //const y_pos = jellyfish.position.y;
+        //const z_pos = jellyfish.position.z;
+
+        /*if(x_pos > screen_width / 2 || x_pos < - screen_width / 2){
+            jellyfish_direction = new THREE.Vector3(
+                -jellyfish_direction.getComponent(0), 
+                jellyfish_direction.getComponent(1), 
+                jellyfish_direction.getComponent(2)
+            );
+        }
+        if(y_pos > screen_height / 2 || y_pos < - screen_height / 2){
+            jellyfish_direction = new THREE.Vector3(
+                jellyfish_direction.getComponent(0), 
+                -jellyfish_direction.getComponent(1), 
+                jellyfish_direction.getComponent(2)
+            );
+        }*/
+
         const angle = time*speed;
-        group.rotation.y = angle;
-        group.rotation.x = angle;
-        group.rotation.z = angle;
+        jellyfish.rotation.y = angle;
+        jellyfish.rotation.x = angle;
+        jellyfish.rotation.z = angle;
+
+        //console.log(jellyfish)
+
+        jellyfish.position.x = jellyfish_direction_x * jellyfish_velocity;
+        jellyfish.position.y = jellyfish_direction_y * jellyfish_velocity;
+        jellyfish.position.z = jellyfish_direction_z * jellyfish_velocity;
+    
+        //console.log("new x: ", jellyfish.position.x )
+        //console.log("new y: ", jellyfish.position.y )
+
     }
 
     return {
